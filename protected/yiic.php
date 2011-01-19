023be7cb17fc5f4d74dd51f767c576299c25169b
Piotr <gnysek@gmail.com>
2011-01-19 10:11:32 +0100

    pierwsza wrzutka

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../../WAMPP/www/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
