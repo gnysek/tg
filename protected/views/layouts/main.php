1f2ebc5a1ac8b4fe42b16ea57a386ce29efb7691
Piotr <gnysek@gmail.com>
2011-02-28 20:09:26 +0100

    - prosty design

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -12,6 +12,7 @@
 	<![endif]-->
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/tg.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
@@ -22,21 +23,32 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo">
+			<div id="logotext">
+				<a href="<?php Yii::app()->createUrl(''); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+			</div>
+			<div id="logomenu">
+				<?php
+				$this->widget('zii.widgets.CMenu', array(
+						'items' => array(
+								array('label' => 'Główna', 'url' => array('/site/index')),
+								array('label' => 'Newsy', 'url' => array('/news')),
+								array('label' => 'Forum', 'url' => array('/forum')),
+								array('label' => 'Gry', 'url' => array('/game')),
+								array('label' => 'Wydawcy', 'url' => array('/publisher')),
+								array('label' => 'Rankingi', 'url' => array('/ranking')),
+//								array('label' => 'About', 'url' => array('/site/page')),
+//								array('label' => 'Kontakt', 'url' => array('/site/contact')),
+								array('label' => 'Admin', 'url' => array('/admin'), 'visible' => Yii::app()->user->admin),
+								array('label' => 'Zaloguj', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+								array('label' => 'Wyloguj (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+						),
+				));
+				?>
+			</div><!-- mainmenu -->
+		<div class="clear"></div>
+		</div><!-- logo -->
 	</div><!-- header -->
-
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'ACP (tymczasowo)', 'url'=>array('/admin'), 'visible'=>Yii::app()->user->admin),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
 
 	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 		'links'=>$this->breadcrumbs,
