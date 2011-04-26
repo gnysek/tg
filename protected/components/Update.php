67ea4b0dbeec57c14cb3bed56f79fa6c82c04374
Piotr <gnysek@gmail.com>
2011-04-26 20:13:54 +0200

    - instalacja tabeli config ;)

--- protected/components/Update.php
+++ protected/components/Update.php
@@ -13,32 +13,42 @@ class Update
 
 	public static function runUpdate()
 	{
-		$model = Config::model()->findByPk('database');
+		try {
+			$model = Config::model()->findByPk('database');
+		} catch (CDbException $e) {
+			$connection = Yii::app()->db;
+			$command = $connection->createCommand("CREATE TABLE IF NOT EXISTS `config` (
+						  `name` varchar(255) NOT NULL,
+						  `value` text,
+						  PRIMARY KEY (`name`)
+						) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+			$command->execute();
+			Yii::app()->request->redirect(Yii::app()->createUrl('/'));
+		}
 		if (!$model) {
 			$model = new Config();
 			$model->name = 'database';
 			$model->value = '1';
 			$model->save();
-		} else {
-			// kolejne updejty
-			if ($model->value == 1) {
-				$connection = Yii::app()->db;
-				$command = $connection->createCommand("ALTER TABLE `game` CHANGE `name` `name` VARCHAR( 255 ) NOT NULL ");
-				$command->execute();
-				$model->value = 2;
-				$model->save();
-			}
-			if ($model->value == 2) {
-				$connection = Yii::app()->db;
-				$command = $connection->createCommand("ALTER TABLE `game` CHANGE `url` `url` VARCHAR( 255 ) NOT NULL");
-				$command->execute();
-				$command = $connection->createCommant("ALTER TABLE `game` ADD FOREIGN KEY ( `publisher_id` ) REFERENCES `tg`.`publisher` (
-						`publisher_id`
-						) ON DELETE CASCADE ON UPDATE CASCADE ;");
-				$command->execute();
-				$model->value = 3;
-				$model->save();
-			}
+		}
+		// kolejne updejty
+		if ($model->value == 1) {
+			$connection = Yii::app()->db;
+			$command = $connection->createCommand("ALTER TABLE `game` CHANGE `name` `name` VARCHAR( 255 ) NOT NULL ");
+			$command->execute();
+			$model->value = 2;
+			$model->save();
+		}
+		if ($model->value == 2) {
+			$connection = Yii::app()->db;
+			$command = $connection->createCommand("ALTER TABLE `game` CHANGE `url` `url` VARCHAR( 255 ) NOT NULL");
+			$command->execute();
+			$command = $connection->createCommand("ALTER TABLE `game` ADD FOREIGN KEY ( `publisher_id` ) REFERENCES `tg`.`publisher` (
+					`publisher_id`
+					) ON DELETE CASCADE ON UPDATE CASCADE ;");
+			$command->execute();
+			$model->value = 3;
+			$model->save();
 		}
 	}
 
